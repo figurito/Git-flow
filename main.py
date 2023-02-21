5185510aee0eb8ccb38f17f689ccbb867e000113
“adsiCasona” <adsisantandercasona@gmail.com>
2023-02-21 11:51:00 -0500

    echo update task

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ def add_task():
     print ("Inicio el Codigo Agregamos la funcion para tarea")
     
     return
+
+''' Función del Segundo Programador '''
+def read_task():  
+    print ("Esta es la función de Consultar Tareas")
+    
+    return
+
+""" rama f_update_task el Primer Programador """
+def update_task():
+    print("Aqui es el codigo de Actualizar tarea")
+    
+    read_task
